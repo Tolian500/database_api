56b370c7f44279eb2c2bd746e533d8c7bb681007
tolian500 <tolian500@gmail.com>
2024-10-10 21:38:18 +0200

    sql_manager update

---
+++ sql_manager.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, select, MetaData, Table, and_, desc
+
+
+def get_last_data(metadata: MetaData, engine: create_engine):
+    tables = ['indoor', 'outdoor']
+    results = {}
+    for table_name in tables:
+        table = Table(
+            table_name,
+            metadata,
+            autoload=True,
+            autoload_with=engine
+        )
+
+        # Modify the select statement to order by 'datetime' in descending order
+        # and limit the result to 1
+        stmt = select([
+            table.columns.temp,  # assuming 'temp' is the correct column for temperature
+            table.columns.hum  # assuming 'hum' is the correct column for humidity
+        ]).order_by(
+            desc(table.columns.datetime)  # ordering by datetime descending
+        ).limit(1)  # limiting the results to one
+
+        # Execute the query
+        connection = engine.connect()
+        cur_results = connection.execute(stmt).fetchall()
+        results[table_name] = cur_results  # store results with table name as key
+
+    return results
